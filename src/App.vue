f3dec4a3cf9f6cbfd358bce7d111beb614957016
Hasin Hayder <countdraculla@gmail.com>
2024-11-26 21:56:52 +0530

    Awesome Alert

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import AwesomeAlert from './components/AwesomeAlert.vue';
+</script>
+
+<template>
+  <AwesomeAlert  heading="Awesome Alert" message="This is Fantastic!" color="red" />
+  <awesome-alert heading="It's Dynamic" message="You can pass anything in props" color="green" />
+  <awesome-alert heading="Upload Failed!" message="File cannot be more than 5MB" color="red" />
+  <awesome-alert heading="Operation successful!" message="Email has been successfully sent" color="green" />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/AwesomeAlert.vue
@@ -0,0 +1,30 @@
+<script setup>
+    // defineProps([
+    //     'heading',
+    //     'message',
+    //     'color'
+    // ])
+
+    defineProps({
+        heading:{
+            type:String,
+            default:'Heading'
+        },
+        message:{
+            type:String,
+            default:'Message'
+        },
+        color:{
+            type:String,
+            default:'indigo'
+        }
+    })
+</script>
+
+<template>
+    <div class="p-4 mb-4 text-sm rounded-lg dark:bg-gray-800" :class="`text-${color}-800 bg-${color}-50`" role="alert">
+        <span class="font-medium"> {{ heading }}</span> {{ message }}
+    </div>
+</template>
+
+<style scoped></style>
